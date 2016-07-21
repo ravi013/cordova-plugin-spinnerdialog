03b952e80b279cf0792a3eb99b3d1ab95b39392d
Filiph Sandström <filiph.sandstrom@filfatstudios.com>
2016-07-21 22:10:51 +0200

    Update SpinnerDialogProxy.js

--- src/windows/SpinnerDialogProxy.js
+++ src/windows/SpinnerDialogProxy.js
@@ -18,6 +18,7 @@ cordova.commandProxy.add("SpinnerDialog",{
                 data.title = undefined;
             progressIndicator.text = typeof data.title !== 'undefined' ? data.title : 'Loading...';
             progressIndicator.showAsync();
+            Windows.UI.ViewManagement.StatusBar.getForCurrentView().showAsync();
         } else if (typeof Windows !== 'undefined' &&
                     typeof Windows.UI !== 'undefined' /* Check that we have a UI to work with */) {
                         
@@ -30,6 +31,7 @@ cordova.commandProxy.add("SpinnerDialog",{
             typeof Windows.UI.ViewManagement.StatusBar !== 'undefined' /* Check that we have the StatusBar to work with*/) {
 
             progressIndicator.hideAsync();
+            Windows.UI.ViewManagement.StatusBar.getForCurrentView().hideAsync(); //TODO
         } else if (typeof Windows !== 'undefined' &&
                     typeof Windows.UI !== 'undefined' /* Check that we have a UI to work with */) {
                         
